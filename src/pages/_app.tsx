28e39f0e2ed277592ae062eebf0119a42c933fa6
Renan Pereira <renanmol87@gmail.com>
2022-05-25 19:36:14 -0300

    feat(styles): add custom chakra theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { AuthProvider } from '@/contexts/AuthContext'
+import { theme } from '@/styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
