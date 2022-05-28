8c63de7aca4819ad8099104d1cd19ff03675ded1
Renan Pereira <renanmol87@gmail.com>
2022-05-28 18:07:31 -0300

    feat: add Global style component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,37 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { css, Global } from '@emotion/react'
+import Head from 'next/head'
+
 import { AuthProvider } from '@/contexts/AuthContext'
 import { theme } from '@/styles/theme'
+
+const GlobalStyle = () => {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+    </>
+  )
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <GlobalStyle />
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
